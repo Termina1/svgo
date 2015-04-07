9c569aebc00366935bbea4f956a23ad500e28de0
Termina1 <terminal2010@gmail.com>
2015-04-07 13:24:12 +0300

    added plugin

---
+++ plugins/removeFill.js
@@ -0,0 +1,38 @@
+'use strict';
+
+exports.type = 'perItem';
+
+exports.active = true;
+
+exports.params = {
+    stroke: true,
+    fill: true
+};
+
+var shape = require('./_collections').elemsGroups.shape,
+    styleOrScript = ['style', 'script'],
+    hasStyleOrScript = false;
+
+/**
+ * Remove useless stroke and fill attrs.
+ *
+ * @param {Object} item current iteration item
+ * @param {Object} params plugin params
+ *
+ * @author Termina1
+ */
+exports.fn = function(item, params) {
+    if (item.isElem(styleOrScript)) {
+        hasStyleOrScript = true;
+    }
+
+    if (!hasStyleOrScript && item.isElem(shape)) {
+        if (item.hasAttr('fill')) {
+            item.attr('fill').value = 'none';
+        }
+        if (item.hasAttr('stroke')) {
+            item.attr('stroke').value = 'none';
+        }
+    }
+
+};
